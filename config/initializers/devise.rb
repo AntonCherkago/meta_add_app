8999c6218939aecdce1d79dd06d9ad7266e648e8
AntonCherkago <antonkonovalov167@gmail.com>
2024-06-20 01:48:54 +0300

    security cahnges

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -304,7 +304,9 @@ Devise.setup do |config|
   # Note: These might become the new default in future versions of Devise.
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'ads_read,ads_management'
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'ads_read,ads_management',
+                                                                                 info_fields: 'email',
+                                                                                 callback_url: 'https://meta-adds-app-a985722603b4.herokuapp.com/users/auth/facebook/callback'
   config.omniauth_path_prefix = '/users/auth'
 
 
