a525ecdf7f5307bf2253abc007b56d5f7a0269ee
AntonCherkago <antonkonovalov167@gmail.com>
2024-06-21 22:02:58 +0300

    fix devise commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -306,7 +306,8 @@ Devise.setup do |config|
   config.responder.redirect_status = :see_other
   config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'ads_read,ads_management',
                                                                                  callback_url: 'https://meta-adds-app-a985722603b4.herokuapp.com/users/auth/facebook/callback',
-                                                                                 response_type: 'code'
+                                                                                 response_type: 'code',
+                                                                                 config_id: '1486861518588875'
 
   # ==> Configuration for :registerable
 
