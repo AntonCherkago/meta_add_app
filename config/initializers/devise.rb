c88b6e76880ed2b5dbb33a90eaf59bf8ebb78921
AntonCherkago <antonkonovalov167@gmail.com>
2024-06-21 22:14:15 +0300

    fix devise commit

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -304,8 +304,7 @@ Devise.setup do |config|
   # Note: These might become the new default in future versions of Devise.
   config.responder.error_status = :unprocessable_entity
   config.responder.redirect_status = :see_other
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], scope: 'ads_read,ads_management',
-                                                                                 callback_url: 'https://meta-adds-app-a985722603b4.herokuapp.com/users/auth/facebook/callback',
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'], callback_url: 'https://meta-adds-app-a985722603b4.herokuapp.com/users/auth/facebook/callback',
                                                                                  response_type: 'code',
                                                                                  config_id: '1486861518588875'
 
